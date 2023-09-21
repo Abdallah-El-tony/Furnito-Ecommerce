230f12e1e4c9968af973f52efb83d7bec8f21b73
AbdullahEltony <abdullahmeltony@gmail.com>
2023-09-21 19:03:07 +0300

    Modified auth

--- src/store/slices/AuthSlice.js
+++ src/store/slices/AuthSlice.js
@@ -7,19 +7,19 @@ const getTotalUsers = async()=>{
     return response.data
 }
 
-let usersLength =JSON.parse(localStorage.getItem('totalUsers')) || 0;
+let usersLength =0;
 
 (async () => {
     try {
       const data = await getTotalUsers();
-      usersLength = data.length;
-      localStorage.setItem('totalUsers',JSON.stringify(usersLength))
+      console.log(data.length)
+      usersLength = data.length
     } catch (error) {
-      console.log(error)
+      // Handle error if necessary
     }
   })();
 
-    const initialState= {
+const initialState= {
     isAuth:JSON.parse(localStorage.getItem('isAuth')) || false,
     userId:JSON.parse(localStorage.getItem('userId')) || usersLength,
     totalUsers:JSON.parse(localStorage.getItem('totalUsers')) || usersLength
